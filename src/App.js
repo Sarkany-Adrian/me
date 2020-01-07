3b75c68b7737b832d4cc3fbc084e401756063bc4
Sarkany Sebastian-Adrian <fw.adrian.sarkany@Sarkanys-MacBook-Pro.local>
2020-01-08 00:52:02 +0100

    Adding the router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 // @flow
 import React from 'react';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Home from './pages/home';
+import About from './pages/about';
 import Header from './components/header';
 import Footer from './components/footer';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <p>content</p>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/about/about.js
@@ -0,0 +1,8 @@
+// @flow
+import React from 'react';
+
+const About = () => {
+  return <div>about page</div>;
+};
+
+export default About;
