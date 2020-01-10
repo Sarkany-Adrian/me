75c5ac15ea00e7ff13632c9b02e6bf966b68301a
Sarkany Sebastian-Adrian <fw.adrian.sarkany@Sarkanys-MacBook-Pro.local>
2020-01-10 18:13:12 +0100

    Progress

---
+++ src/atoms/logo/logo.js
@@ -0,0 +1,8 @@
+// @flow
+import React from 'react';
+
+const Logo = () => {
+  return <div>some logo hers</div>;
+};
+
+export default Logo;
--- src/index.js
+++ src/index.js
@@ -5,26 +5,35 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 //
 import './index.css';
+// pages
 import Home from './pages/home';
 import About from './pages/about';
 import Error404 from './pages/error-404';
+
+// layout todo: refactor this into it's own wrapper
+import TopHat from './components/top-hat';
+import Header from './components/header';
 
 const appContainer: HTMLElement | null =
   document && document.getElementById('root');
 
 if (appContainer) {
   ReactDOM.render(
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
+    <div>
+      <TopHat />
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
 
-        <Route path="/about" component={About} />
-        {/* Error page */}
-        <Route path="/error" component={Error} />
-        {/* 404 */}
-        <Route path="*" component={Error404} />
-      </Switch>
-    </BrowserRouter>,
+          <Route path="/about" component={About} />
+          {/* Error page */}
+          <Route path="/error" component={Error} />
+          {/* 404 */}
+          <Route path="*" component={Error404} />
+        </Switch>
+      </BrowserRouter>
+    </div>,
     appContainer
   );
 }
