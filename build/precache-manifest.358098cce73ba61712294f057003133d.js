0a836859312d9d8141d112546ab5ba8ef268cd45
Sarkany Sebastian-Adrian <fw.adrian.sarkany@Sarkanys-MacBook-Pro.local>
2020-02-20 14:14:22 +0100

    Tweaks

---
+++ build/precache-manifest.358098cce73ba61712294f057003133d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b369b76bb36ad34b67cf3350ef966cdb",
+    "url": "/di/index.html"
+  },
+  {
+    "revision": "e2aa6696a58d504866a5",
+    "url": "/di/static/css/2.42f96b38.chunk.css"
+  },
+  {
+    "revision": "2549e5e1372cecdeaf66",
+    "url": "/di/static/css/main.1b16b34d.chunk.css"
+  },
+  {
+    "revision": "e2aa6696a58d504866a5",
+    "url": "/di/static/js/2.ac12a743.chunk.js"
+  },
+  {
+    "revision": "edfcf8e67a28338e04654624ea455515",
+    "url": "/di/static/js/2.ac12a743.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2549e5e1372cecdeaf66",
+    "url": "/di/static/js/main.27f8f57b.chunk.js"
+  },
+  {
+    "revision": "376b27d2d5cd6abd4f2e",
+    "url": "/di/static/js/runtime-main.6cfbb1a0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/di/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/di/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/di/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/di/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/di/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "298f8573365179bf55c52316c94c7b5f",
+    "url": "/di/static/media/hero.298f8573.jpg"
+  }
+]);
